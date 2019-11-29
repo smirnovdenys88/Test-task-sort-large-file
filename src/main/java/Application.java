cfd30fe35e1ab1ba5451f226a61f4214feee4138
ShadrinaTatiana <newbookit@gmail.com>
2019-11-30 00:27:40 +0200

    improved

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,3 +1,5 @@
+import javafx.beans.binding.StringBinding;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -9,22 +11,22 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+import java.util.function.Predicate;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class Application {
     private static Logger logger = Logger.getLogger(Application.class.getName());
-    private static final int DEFAULT_BUFFER_SIZE = 1024 * 1; // 1 MB
+    private static final int DEFAULT_BUFFER_SIZE = 1024; // 1 MB
+//    private static final int FILE_SEGMENT_SIZE = 1024; // 1 MB
     private static final int FILE_SEGMENT_SIZE = 1024; // 1 MB
-    private static final int FILE_SIZE = 1024 * 16; // 10 MB
+    private static final int FILE_SIZE = 1024 * 1024; // 10 MB
     private static Random random = new Random();
     private static int j = 0;
     private static int i = 0;
-    private static String path = "/home/user/IdeaProjects/Learning/sort-large-file/src/main/resources";
-    private static String pathOutFile = "/home/user/IdeaProjects/Learning/sort-large-file/src/main/resources/out.txt";
+    private static String path = "/home/ann/IdeaProjects/Test-task-sort-large-file/src/main/resources";
+    private static String pathOutFile = path + "/out.txt";
     private static List<File> files = new ArrayList<>();
 
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -38,22 +40,37 @@ public class Application {
         logger.info("Start process createLargeFile finish");
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File(path + "/data.txt")));
         String str = "";
-        while (str.length() < FILE_SIZE) {// 120 MB
 
-            bufferedWriter.write(str);
-            bufferedWriter.flush();
+        Set<Integer> set = new HashSet<>();
+        while (set.size() < FILE_SIZE){
             if (str.length() == FILE_SIZE) {
-                str += random.nextInt(Integer.MAX_VALUE);
+                set.add(random.nextInt(Integer.MAX_VALUE));
             } else {
-                str += random.nextInt(Integer.MAX_VALUE) + "\n";
+                set.add(random.nextInt(Integer.MAX_VALUE));
             }
         }
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(set.stream().map(s -> s.toString()).collect(Collectors.joining("\n")));
+        bufferedWriter.write(stringBuilder.toString());
         bufferedWriter.close();
-        logger.info("Finish process createLargeFile");
+
+//        while (str.length() < FILE_SIZE) {// 120 MB
+//
+//            bufferedWriter.write(str);
+//            bufferedWriter.flush();
+//            if (str.length() == FILE_SIZE) {
+//                str += random.nextInt(Integer.MAX_VALUE);
+//            } else {
+//                str += random.nextInt(Integer.MAX_VALUE) + "\n";
+//            }
+//        }
+//        bufferedWriter.close();
+//        logger.info("Finish process createLargeFile");
     }
 
     static void cutFile() throws IOException {
         logger.info("Start process cutFile");
+
         FileChannel source = new FileInputStream(new File(path + "/data.txt")).getChannel();
         ByteBuffer buf = ByteBuffer.allocateDirect(DEFAULT_BUFFER_SIZE);
         File file = new File(path + "/temp-" + i + ".txt");
@@ -85,7 +102,6 @@ public class Application {
         logger.info("Start process sort by files");
         List<Integer> integers = new ArrayList<>();
 
-        //Sort ever file
         for (j = 0; j <= i; j++) {
             BufferedReader br = new BufferedReader(new FileReader(files.get(j)));
             try {
@@ -129,25 +145,60 @@ public class Application {
         boolean flag = true;
         FileWriter fr = new FileWriter(out, true);
 
+        Map<Integer,Integer> indexValue = new HashMap<>();
+
+        for(j = 0; j <= i; j++){
+            indexValue.put(j, -1);
+        }
+
+        Map<Integer,Integer> indexValueTree;
+        String value;
+        int stopRead = 0;
+        int x = 0;
+        boolean increment = false;
+
         while (flag) {
-            String line;
-            for (j = 0; j <= i; j++) {
+            for (j = 0; j <= i; j++)  {
                 try {
-                    while ((line = brs.get(j).readLine()) != null) {
-                        fr.write(line);
-                        fr.write("\n");
-                        fr.flush();
+                    if(indexValue.get(j) > -1) continue;
+
+                    value = brs.get(j).readLine();
+                    if(value == null){
+                        stopRead++;
+                        continue;
+                    }
+                    if(stopRead == i){
+                        flag = false;
                         break;
                     }
-                    if (j == 0) {
-                        flag = line != null;
-                    }
+                    indexValue.put(j, Integer.valueOf(value));
                 } catch (IOException e) {
                     System.err.format("IOException: %s%n", e);
                 }
             }
+
+            indexValueTree = indexValue.entrySet().stream()
+                    .sorted(Map.Entry.comparingByValue())
+                    .limit(13)
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+
+            if(indexValueTree.isEmpty()) break;
+            final Integer finalX = x;
+            if(!indexValueTree.values().stream().filter(s -> s.equals(finalX)).findFirst().isPresent()) x++;
+
+            for (Map.Entry<Integer,Integer> intTree: indexValueTree.entrySet()){
+                if(x != intTree.getValue()) continue;
+                x = intTree.getValue();
+                fr.write(intTree.getValue());
+                fr.write("\n");
+                fr.flush();
+                indexValue.put(intTree.getKey(), -1);
+            }
+
             integers.clear();
         }
+
+        stopReadFile:
 
         for (BufferedReader reader : brs) {
             reader.close();
@@ -161,3 +212,4 @@ public class Application {
         logger.info("Finish process merge file");
     }
 }
+
